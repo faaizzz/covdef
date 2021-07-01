6c20d0e5d11e7a746137f080d26a26911518505a
Faaiz Rissad <fffaaiz@gmail.com>
2021-07-01 18:39:25 +0530

    Added Meterial modules

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { State } from 'src/app/_models/state';
 import { District } from 'src/app/_models/district';
 import { FormBuilder, Validators } from '@angular/forms';
 import { CowinApiService } from 'src/app/_services/cowin-api.service';
 import { Session } from 'src/app/_models/session';
 import { formatDate } from '@angular/common';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
   name: string;
@@ -33,10 +34,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent  implements OnInit  {
+export class HomeComponent  implements OnInit,AfterViewInit  {
 
   displayedColumns: string[] = ['date','name', 'pincode','min_age_limit','vaccine', 'available_capacity_dose1','available_capacity_dose2','fee'];
-  dataSource = ELEMENT_DATA;
+  // dataSource = ELEMENT_DATA;
+  dataSource: MatTableDataSource<Session>;
 
   states: State[] = [];
   districts: District[] = [];
@@ -62,14 +64,33 @@ export class HomeComponent  implements OnInit  {
 
   hasUnitNumber = false;
 
-  constructor(private fb: FormBuilder,private cowinApiService: CowinApiService) {
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(private fb: FormBuilder,private cowinApiService: CowinApiService) {
+    // this.autoSearch();
+    // this.dataSource = new MatTableDataSource(this.sessions);
   }
   ngOnInit(): void {
     // this.getStates();
     this.autoSearch();
     // this.cowinApiService.findSlotsTomorrow("307");    
   }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+     
+     } this.dataSource.paginator.firstPage();
+    }
 
   onSubmit(): void {
 
@@ -112,11 +133,12 @@ export class HomeComponent  implements OnInit  {
         .filter(
           x=>x.available_capacity > 0
           && x.fee == "0"
-          && x.vaccine == "COVISHIELD"
+          // && x.vaccine == "COVISHIELD"
           // && x.max_age_limit == 45
           ); 
         this.sessions = this.sessions.concat(filtredSlots);
         this.sessions = this.sessions.sort((a,b)=> (b.available_capacity - a.available_capacity));
+        this.dataSource = new MatTableDataSource<Session>(this.sessions);
       })
       
       console.log("Inside subscribe");      
