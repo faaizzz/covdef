e01edad3e7bffe3f1b6f2cf4dea35cbde6a43093
Faaiz Rissad <fffaaiz@gmail.com>
2021-07-07 10:57:05 +0530

    Added Live Time

--- src/app/_pages/nav/nav.component.ts
+++ src/app/_pages/nav/nav.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit, OnDestroy  {
+
+  todaysDataTime = '';
+  timer: number = 0;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -17,5 +21,24 @@ export class NavComponent {
     );
 
   constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.startTimer();
+  }
+  ngOnDestroy(): void {
+    this.stopTimer();
+  }
+
+  startTimer() {
+    let today = new Date();
+    this.todaysDataTime = formatDate(today, 'hh:mm:ss a', 'en-US', '+0530');
+    this.timer = window.setInterval(() => {
+      today = new Date();
+      this.todaysDataTime = formatDate(today, 'hh:mm:ss a', 'en-US', '+0530');
+    }, (1000));
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+  }
 
 }
